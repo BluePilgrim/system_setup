e45a0d8b4db19388635097c3c2ca39d37f3390a1
David Yang <david.yang@mentisware.com>
2015-07-09 14:45:24 +0900

    Now, boot2docker VM works in streamline with the server. boot2docker ip returns an reachable IP address.

--- system_setup/docker_module/cluster/mesos/boot2docker-mesos-slave.sh
+++ system_setup/docker_module/cluster/mesos/boot2docker-mesos-slave.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-docker run -d -p 5051:5051 --name=mesos-slave --net=host --privileged -v /var/run/docker.sock:/var/run/docker.sock -v /sys:/sys -v $(which docker):/usr/local/bin/docker cluster/mesos mesos-slave --master=zk://${ZOOKEEPER}/mesos --containerizers=docker,mesos --executor_registration_timeout=5mins --ip=$(boot2docker ip)
+
+ZOOKEEPER=${PRIMARY_SERVER}:2181
+PUBLIC_IP=$(boot2docker ip)
+
+docker run -d -p 5051:5051 --name=mesos_slave1 --net=host --privileged -v /var/run/docker.sock:/var/run/docker.sock -v /sys:/sys -v /var/lib/docker:/var/lib/docker -e PUBLIC_IP=${PUBLIC_IP} -e ZOOKEEPER=${ZOOKEEPER} cluster/mesosphere /usr/local/bin/mesos-slave_bootstrap.sh
