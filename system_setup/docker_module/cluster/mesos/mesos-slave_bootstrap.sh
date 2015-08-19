6727e56f0cd838b384e47a293e0f2f5801992e55
David Yang <david.yang@mentisware.com>
2015-08-19 00:31:27 +0000

    removed the erraneous previous files.

--- system_setup/docker_module/cluster/mesos/mesos-slave_bootstrap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# ZOOKEEPER and PUBLIC_IP will be given via -e option of docker run.
-
-: ${MESOS_SLAVE_OPTS:="--ip=${PUBLIC_IP} --master=zk://${ZOOKEEPER}/mesos --containerizers=docker,mesos --executor_registration_timeout=5mins"}
-
-mesos-slave $MESOS_SLAVE_OPTS
--- system_setup/docker_module/research/build_env.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker build -t research/base - < research.dockerfile
-docker build -t research/openjdk - < openjdk.dockerfile
-#docker build -t research/openjdk_build - < openjdk_build.dockerfile
